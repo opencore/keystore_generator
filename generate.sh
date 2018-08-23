06894b6ce076a67607b25208baa3c917f8e6150d
Soenke Liebau <soenke.liebau@opencore.com>
2018-08-23 11:43:44 +0200

    Removed sliebau username Fixed random stuff:  - removal of keystores & certificates  - "creates" clause for creating keystores  - added uncommented variables to inventory

--- generate.sh
+++ generate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ -z $1 ]; then
-  echo "Please specify a name for this set of keystores!"
+  echo "Please specify an inventory file to generate this set of keystores from!"
   exit 1
 fi
 
 ansible-galaxy install -r requirements.yml
 dir=$(pwd)
-ansible-playbook run.yml -i inventory --extra-vars "KEYSTORE_BASE_DIR=${dir}/$1"
+ansible-playbook run.yml -i $1 --extra-vars "KEYSTORE_BASE_DIR=${dir}/$1.keystores"
